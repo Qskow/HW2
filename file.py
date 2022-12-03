4dca8167c34735cf75a9bd3dff7c8484fe7ece52
Anton Kuskov <AntonKuskov123@yandex.ru>
2022-12-03 06:07:47 +0300

    HW2

---
+++ file.py
@@ -0,0 +1,34 @@
+#1
+
+print("Введите первое число:")
+a = float(input())
+print("Введите второе число:")
+b = float(input())
+
+print(f"Сумма a и b: {a + b}")
+print(f"Разность a и b: {a - b}")
+print(f"Произведение a и b: {a*b}")
+print(f"Частное a и b: {a/b}")
+print(f"Число a в степени b: {a**b}")
+print(f"Деление числа a по модулю b: {a%b}")
+print(f"Целочисленное деление a на b: {a//b}")
+print("")
+
+#2
+
+print("Как вас зовут?")
+name = input(str())
+print(f"Зравствуйте, {name}!")
+print("")
+
+#3
+
+my_string = input("Введите строку: ")
+print(my_string[-1] + my_string[-2])
+print("")
+#4
+
+from math import pi
+print("Введите радиус круга для вычисления его площади: ")
+r = float(input())
+print(f"Площадь круга равна: {pi * r}")
